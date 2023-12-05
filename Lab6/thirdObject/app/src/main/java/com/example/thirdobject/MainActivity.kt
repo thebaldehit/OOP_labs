8057fb81d58e0d3d2848031aedaede7ef177ea55
TheBladeHit <boqdanyarmolka@gmail.com>
2023-12-05 12:57:47 +0200

    Impelemnted all functionality for 6 lab

--- Lab6/thirdObject/app/src/main/java/com/example/thirdobject/MainActivity.kt
+++ Lab6/thirdObject/app/src/main/java/com/example/thirdobject/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.thirdobject
 
 import android.content.ClipboardManager
-import android.content.Context
+import android.graphics.Color
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.github.mikephil.charting.charts.LineChart
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet
 import com.example.thirdobject.databinding.ActivityMainBinding
+import com.github.mikephil.charting.components.Description
+import com.github.mikephil.charting.components.YAxis
 import java.util.Objects
 
 
@@ -23,7 +30,34 @@ class MainActivity : AppCompatActivity() {
         if (hasFocus) {
             val clipboard = getSystemService(CLIPBOARD_SERVICE) as ClipboardManager
             val item = Objects.requireNonNull(clipboard.primaryClip)?.getItemAt(0)
-            binding.text.text = item?.text
+
+            val description = Description()
+            description.text = "Data"
+            binding.lineChart.description = description
+
+
+            val deserializedData = item?.text?.split(" ")
+
+            val entries1 = ArrayList<Entry>()
+            for (index in deserializedData!!.indices) {
+                entries1.add(Entry(index.toFloat(), deserializedData[index].toFloat()))
+            }
+
+            val dataset = LineDataSet(entries1, "Our data")
+            dataset.setColor(Color.BLUE)
+
+            val xAxis: XAxis = binding.lineChart.xAxis
+            val yAxis: YAxis = binding.lineChart.axisLeft
+
+            xAxis.position = XAxis.XAxisPosition.BOTTOM
+            yAxis.axisMinimum = 0f
+            yAxis.setDrawGridLines(true)
+            yAxis.setDrawAxisLine(true)
+
+            val lineData = LineData(dataset)
+
+            binding.lineChart.data = lineData
+            binding.lineChart.invalidate()
         }
     }
 }
